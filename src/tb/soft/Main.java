27bd9ccd5adfb7698af1055a858e8a2893b76993
Quarol <karol.charchut@gmail.com>
2022-11-20 23:10:10 +0100

    Refactor of Main

---
+++ src/tb/soft/Main.java
@@ -0,0 +1,11 @@
+package tb.soft;
+import java.util.HashMap;
+
+public class Main {
+    private static final HashMap<String, String> usersData = new HashMap<String, String>();
+
+    public static void main(String[] args) {
+        LogInFrame logInFrame = new LogInFrame();
+        RegisterFrame registerFrame = new RegisterFrame();
+    }
+}
